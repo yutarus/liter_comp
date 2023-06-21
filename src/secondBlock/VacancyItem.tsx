d963ab3a9b49d7e62e3fdbc4cdb5ad10f3893eed
AlexseyPivoshencko <pivoshenckoalexsey@gmail.com>
2023-06-21 19:31:18 +0300

    Second block. Add files: models.ts, secondBlock.css, secondBlock.tsx, vacancies.data.ts, VacancyItem.tsx; Add folders: public, secondBlock

---
+++ src/secondBlock/VacancyItem.tsx
@@ -0,0 +1,36 @@
+import { IVacancy } from './models'
+
+interface VacacyProps {
+	vacancy: IVacancy
+}
+
+function VacancyItem({ vacancy }: VacacyProps) {
+	return (
+		<div id={'card_' + vacancy.id} className='vacancy__item'>
+			<div
+				className='item__image'
+				style={{
+					backgroundImage: `url(${vacancy.imagePath})`
+				}}
+			>
+				<div className='image__logo'>
+					<div
+						className='logo__svg'
+						style={{
+							backgroundImage: `url(${vacancy.logoPath})`
+						}}
+					></div>
+				</div>
+			</div>
+			<p className='item__title'>{vacancy.title}</p>
+			<ul className='items__terms'>
+				<li>{vacancy.salary}</li>
+				<li>{vacancy.amountWork}</li>
+				<li>{vacancy.place}</li>
+			</ul>
+			<button className='item__btn button__font'>Побачити більше</button>
+		</div>
+	)
+}
+
+export default VacancyItem
